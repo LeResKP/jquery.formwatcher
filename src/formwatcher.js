232cc5ef17909960a90343326d9d4aa7f844b0a3
Aurélien Matouillot <a.matouillot@gmail.com>
2014-04-22 07:53:28 +0200

    Initial version of the plugin: Put events on the inputs of a forms to check when a value has changed.

--- src/formwatcher.js
+++ src/formwatcher.js
@@ -7,32 +7,72 @@
  */
 
 (function($) {
+    "use strict";
 
-  // Collection method.
-  $.fn.formwatcher = function() {
-    return this.each(function(i) {
-      // Do something awesome to each selected element.
-      $(this).html('awesome' + i);
-    });
-  };
+    var FormWatcher = function(element) {
+        this.init(element);
+    };
 
-  // Static method.
-  $.formwatcher = function(options) {
-    // Override default options with passed-in options.
-    options = $.extend({}, $.formwatcher.options, options);
-    // Return something awesome.
-    return 'awesome' + options.punctuation;
-  };
+    FormWatcher.uniqueId = 0;
 
-  // Static method default options.
-  $.formwatcher.options = {
-    punctuation: '.'
-  };
+    FormWatcher.prototype.init = function(element) {
+        this.$element = $(element);
+        this.$inputs = $(element).find(':input');
+        this.$updatedElements = [];
 
-  // Custom selector.
-  $.expr[':'].formwatcher = function(elem) {
-    // Is this element awesome?
-    return $(elem).text().indexOf('awesome') !== -1;
-  };
+        var that = this;
+        this.$inputs.each(function() {
+            var $this = $(this);
+            $this.data('initial.formwatcher', that.getVal($this));
+        });
+
+        this.$inputs.on('change.formwatcher keyup.formwatcher', function() {
+            that.checkChange($(this));
+        });
+    };
+
+    FormWatcher.prototype.getVal = function(element) {
+        if (element.is('[type="checkbox"],[type="radio"]')) {
+            return element.is(':checked');
+        }
+        return element.val();
+    };
+
+    FormWatcher.prototype.checkChange = function(element) {
+        if (this.getVal(element) !== element.data('initial.formwatcher')) {
+            var id = element.data('uniqueId.formwatcher');
+            if (typeof id === 'undefined') {
+                element.data('uniqueId.formwatcher', (id=FormWatcher.uniqueId++));
+            }
+            if(this.$updatedElements.indexOf(id) === -1 ) {
+                this.$updatedElements.push(id);
+                this.$element.trigger('dirty.formwatcher');
+            }
+        }
+        else {
+            var index = this.$updatedElements.indexOf(element.data('uniqueId.formwatcher'));
+            if( index !== -1 ) {
+                this.$updatedElements.pop(index);
+                if(this.$updatedElements.length === 0) {
+                    this.$element.trigger('clean.formwatcher');
+                }
+            }
+        }
+    };
+
+    $.fn.formwatcher = function(option) {
+        return this.each(function() {
+            var $this = $(this);
+            var data = $this.data('formwatcher');
+            if (!data) {
+                $this.data('formwatcher', (data=new FormWatcher(this)));
+            }
+            if (typeof option === 'string') {
+                 data[option]();
+            }
+        });
+    };
+
+    $.fn.formwatcher.Constructor = FormWatcher;
 
 }(jQuery));
--- test/formwatcher_test.js
+++ test/formwatcher_test.js
@@ -1,4 +1,8 @@
 (function($) {
+    "use strict";
+
+    var FormWatcher = $.fn.formwatcher.Constructor;
+
   /*
     ======== A Handy Little QUnit Reference ========
     http://api.qunitjs.com/
@@ -20,43 +24,135 @@
       throws(block, [expected], [message])
   */
 
-  module('jQuery#formwatcher', {
-    // This will run before each test in this module.
-    setup: function() {
-      this.elems = $('#qunit-fixture').children();
-    }
-  });
+    module('jQuery#formwatcher', {
+        // This will run before each test in this module.
+        // setup: function() {
+        //   this.elems = $('#qunit-fixture').children();
+        // }
+    });
 
-  test('is chainable', function() {
-    expect(1);
-    // Not a bad test to run on collection methods.
-    strictEqual(this.elems.formwatcher(), this.elems, 'should be chainable');
-  });
+    test('init', function() {
+        expect(10);
+        var form = $('#form');
+        var fw = new FormWatcher(form);
+        strictEqual(fw.$element.attr('id'), form.attr('id'));
+        strictEqual(fw.$inputs.length, 7);
+        var dirty_cnt = 0;
+        var clean_cnt = 0;
+        form.bind('dirty.formwatcher', function() {
+            dirty_cnt += 1;
+        });
 
-  test('is awesome', function() {
-    expect(1);
-    strictEqual(this.elems.formwatcher().text(), 'awesome0awesome1awesome2', 'should be awesome');
-  });
+        form.bind('clean.formwatcher', function() {
+            clean_cnt += 1;
+        });
 
-  module('jQuery.formwatcher');
+        var $input = $(fw.$inputs[0]);
+        $input.trigger('keyup');
+        strictEqual(dirty_cnt, 0);
+        strictEqual(clean_cnt, 0);
 
-  test('is awesome', function() {
-    expect(2);
-    strictEqual($.formwatcher(), 'awesome.', 'should be awesome');
-    strictEqual($.formwatcher({punctuation: '!'}), 'awesome!', 'should be thoroughly awesome');
-  });
+        $input.val('hello');
+        $input.trigger('keyup');
+        strictEqual(dirty_cnt, 1);
+        strictEqual(clean_cnt, 0);
 
-  module(':formwatcher selector', {
-    // This will run before each test in this module.
-    setup: function() {
-      this.elems = $('#qunit-fixture').children();
-    }
-  });
+        // The form is already dirty
+        $input.trigger('change');
+        strictEqual(dirty_cnt, 1);
+        strictEqual(clean_cnt, 0);
 
-  test('is awesome', function() {
-    expect(1);
-    // Use deepEqual & .get() when comparing jQuery objects.
-    deepEqual(this.elems.filter(':formwatcher').get(), this.elems.last().get(), 'knows awesome when it sees it');
-  });
+        $input.val('Hello world');
+        $input.trigger('keyup');
+        strictEqual(dirty_cnt, 1);
+        strictEqual(clean_cnt, 1);
+    });
+
+    test('getVal', function() {
+        expect(7);
+        var form = $('#form');
+        var fw = new FormWatcher(form);
+
+        var $input = $(fw.$inputs[0]);
+        var $radio1 = $(fw.$inputs[1]);
+        var $radio2 = $(fw.$inputs[2]);
+        var $checkbox1 = $(fw.$inputs[3]);
+        var $checkbox2 = $(fw.$inputs[4]);
+        var $select1 = $(fw.$inputs[5]);
+        var $select2 = $(fw.$inputs[6]);
+
+        var res = fw.getVal($input);
+        strictEqual(res, 'Hello world');
+
+        res = fw.getVal($radio1);
+        strictEqual(res, false);
+
+        res = fw.getVal($radio2);
+        strictEqual(res, true);
+
+        res = fw.getVal($checkbox1);
+        strictEqual(res, false);
+
+        res = fw.getVal($checkbox2);
+        strictEqual(res, true);
+
+        res = fw.getVal($select1);
+        strictEqual(res, '');
+
+        res = fw.getVal($select2);
+        strictEqual(res, 's4');
+    });
+
+    test('checkChange', function() {
+        expect(15);
+        var form = $('#form');
+        var fw = new FormWatcher(form);
+
+        var $radio1 = $(fw.$inputs[1]);
+        var $checkbox1 = $(fw.$inputs[3]);
+
+        var dirty_cnt = 0;
+        var clean_cnt = 0;
+        form.bind('dirty.formwatcher', function() {
+            dirty_cnt += 1;
+        });
+
+        form.bind('clean.formwatcher', function() {
+            clean_cnt += 1;
+        });
+
+        $radio1.prop('checked', true);
+        $radio1.trigger('change');
+        strictEqual(dirty_cnt, 1);
+        strictEqual(clean_cnt, 0);
+        strictEqual(fw.$updatedElements.length, 1);
+        strictEqual(fw.$updatedElements[0], 1);
+
+        // The form is already dirty
+        $radio1.trigger('change');
+        strictEqual(dirty_cnt, 1);
+        strictEqual(clean_cnt, 0);
+        strictEqual(fw.$updatedElements.length, 1);
+        strictEqual(fw.$updatedElements[0], 1);
+
+        $radio1.prop('checked', false);
+        $radio1.trigger('change');
+        strictEqual(dirty_cnt, 1);
+        strictEqual(clean_cnt, 1);
+        strictEqual(fw.$updatedElements.length, 0);
+
+        $checkbox1.prop('checked', true);
+        $checkbox1.trigger('change');
+        strictEqual(dirty_cnt, 2);
+        strictEqual(clean_cnt, 1);
+        strictEqual(fw.$updatedElements.length, 1);
+        strictEqual(fw.$updatedElements[0], 2);
+    });
+
+    test('plugin', function() {
+        expect(1);
+        var fw = $('#form').formwatcher();
+        ok(fw.data('formwatcher'));
+    });
 
 }(jQuery));
